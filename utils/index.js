92cdbf9964e3a1dd48ee9560b60936e64e0a12e6
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-05-19 19:35:03 +0530

    :heavy_minus_sign: refactor: removed api call to get city id and used geolocation instead

--- utils/index.js
+++ utils/index.js
@@ -3,20 +3,21 @@
 
 import { API_KEY } from 'react-native-dotenv'
 
-export function getCityIdAPI (city) {
-    return fetch('https://developers.zomato.com/api/v2.1/cities?q=' + city, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'user-key': API_KEY
-        }
-    })
-    .then(response => response.json())
-}
+// export function getCityIdAPI (lat, lon) {
+//     console.log(`https://developers.zomato.com/api/v2.1/cities?lat=${lat}&lon=${lon}`)
+//     return fetch(`https://developers.zomato.com/api/v2.1/cities?lat=${lat}&lon=${lon}`, {
+//         method: 'GET',
+//         headers: {
+//             'Content-Type': 'application/json',
+//             'user-key': API_KEY
+//         }
+//     })
+//     .then(response => response.json())
+// }
 
-export function getCollectionsAPI(city_id) {
-    console.log('https://developers.zomato.com/api/v2.1/collections?city_id=' + city_id)
-    return fetch('https://developers.zomato.com/api/v2.1/collections?city_id=' + city_id, {
+export function getCollectionsAPI(lat, lon) {
+    console.log(`https://developers.zomato.com/api/v2.1/collections?lat=${lat}&lon=${lon}`)
+    return fetch(`https://developers.zomato.com/api/v2.1/collections?lat=${lat}&lon=${lon}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
