33ea044d170ba629316b98a80cdb66940c1ed3db
AdithyaBhat17 <adithyabhat17.ab@gmail.com>
2019-05-21 19:53:11 +0530

    :tada: fetched restaurant data based on search query

--- components/Restaurants.js
+++ components/Restaurants.js
@@ -1,22 +1,49 @@
 import React from 'react'
-import { View, Text, TextInput, StyleSheet, Dimensions } from 'react-native'
+import { View, Text, Image, TextInput, StyleSheet, Dimensions } from 'react-native'
+import { getRestaurantsAPI, getCityIdAPI } from '../utils';
 
 export default class Restaurants extends React.Component {
     constructor(props) {
         super(props)
         this.state = {            
-            search: '🔎 for restaurants and hit enter...'
+            search: '',
+            id: undefined,
+            restaurants: null
         }
     }
 
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(
+            position => getCityIdAPI(position.coords.latitude, position.coords.longitude)
+                        .then(data => console.log(data.location_suggestions[0].id) || this.setState({id: data.location_suggestions[0].id}))
+        )
+    }
+
+    searchRestaurants = (restaurant) => {
+        const {id} = this.state
+        if(restaurant.trim().length === 0)
+            return alert('Please enter a valid restaurant name!')
+        id && getRestaurantsAPI(id, restaurant)
+        .then(data => this.setState({restaurants: data.restaurants}))        
+    }
+
     render() {
+        const { search } = this.state
         return (
-            <View style={{flexDirection:'row', justifyContent: 'center'}}>
+            <View style={styles.container}>
                 <TextInput 
                     style={styles.search}
                     onChangeText={(search) => this.setState({search})}
-                    value={this.state.search}
+                    placeholder='🔎 for restaurants and hit enter...'
+                    onSubmitEditing={() => this.searchRestaurants(search)}
                 />
+                <View style={styles.imageView}>
+                    { search.trim().length === 0 && 
+                    <Image style={styles.image}
+                     resizeMode='contain' 
+                     source={require('../assets/restaurants.png')} 
+                    />} 
+                </View>
             </View>
         )
     }
@@ -26,13 +53,23 @@ const { width, height } = Dimensions.get('window')
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1
+        flex: 1,
+        alignItems: 'center',
     },
     search: {
+        marginTop: 20,
         width: width * 0.9,
         borderWidth: 1,
         borderRadius: 6,
-        marginTop: 10,
         padding: 10
     },
+    image: {
+        width: width,
+        height: height * 0.8,
+    },  
+    imageView: {
+        flex: 1,
+        flexDirection: 'row', 
+        justifyContent: 'center'
+    }
 })
--- utils/index.js
+++ utils/index.js
@@ -1,23 +1,35 @@
 // https://developers.zomato.com/api/v2.1/cities
 // https://developers.zomato.com/api/v2.1/categories
+// https://developers.zomato.com/api/v2.1/search?entity_id=84&entity_type=city&q=*RESTAURANT_URL*&category=7%2C9
 
 import { API_KEY } from 'react-native-dotenv'
 
-// export function getCityIdAPI (lat, lon) {
-//     console.log(`https://developers.zomato.com/api/v2.1/cities?lat=${lat}&lon=${lon}`)
-//     return fetch(`https://developers.zomato.com/api/v2.1/cities?lat=${lat}&lon=${lon}`, {
-//         method: 'GET',
-//         headers: {
-//             'Content-Type': 'application/json',
-//             'user-key': API_KEY
-//         }
-//     })
-//     .then(response => response.json())
-// }
+export function getCityIdAPI (lat, lon) {
+    console.log(`https://developers.zomato.com/api/v2.1/cities?lat=${lat}&lon=${lon}`)
+    return fetch(`https://developers.zomato.com/api/v2.1/cities?lat=${lat}&lon=${lon}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-key': API_KEY
+        }
+    })
+    .then(response => response.json())
+}
 
 export function getCollectionsAPI(lat, lon) {
-    console.log(`https://developers.zomato.com/api/v2.1/collections?lat=${lat}&lon=${lon}`)
     return fetch(`https://developers.zomato.com/api/v2.1/collections?lat=${lat}&lon=${lon}`, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'user-key': API_KEY
+        }
+    })
+    .then(response => response.json())
+}
+
+export function getRestaurantsAPI(city_id, restaurant) {
+    console.log(`https://developers.zomato.com/api/v2.1/search?entity_type=city&entity_id=${city_id}&q=${restaurant}`)
+    return fetch(`https://developers.zomato.com/api/v2.1/search?entity_type=city&entity_id=${city_id}&q=${restaurant}`, {
         method: 'GET',
         headers: {
             'Content-Type': 'application/json',
